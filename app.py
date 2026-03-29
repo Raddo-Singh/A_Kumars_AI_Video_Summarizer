bffe78d583a41365c846384e0de409f728175361
A Kumar <raddosingh@gmail.com>
2026-03-30 00:23:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 from main import video_to_summary
 
 def main():
-    st.title("A Kumar's Video Summarizer AI")
+    st.title("A Kumar's AI Video Summarizer")
 
     uploaded_file = st.file_uploader(
         "Upload a video file", type=["mp4", "mov", "avi", "mkv"]
@@ -18,7 +18,7 @@ def main():
             temp_video.write(uploaded_file.read())
             temp_video_path = temp_video.name
 
-        # Run the video summarizer
+        # Run video summarization
         summary_result = video_to_summary(
             video_path=temp_video_path,
             model_size="base",
